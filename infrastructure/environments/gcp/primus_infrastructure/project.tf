b91fd684879938c464e92839ac0765dad8d04953
Jonathan Rainer <jonathanrainer@monzo.com>
2024-04-20 14:32:21 +0100

    Add Terraform to give a location of Primus to reside

---
+++ infrastructure/environments/gcp/primus_infrastructure/project.tf
@@ -0,0 +1,16 @@
+data "google_billing_account" "pmqs_cloud_billing_account" {
+  billing_account = "01F9E6-4D1C8A-58271E"
+}
+
+data "google_folder" "primus" {
+  folder              = "66231054798"
+  lookup_organization = true
+}
+
+resource "google_project" "pmqs_cloud_foundation" {
+  name                = "Primus Infrastructure"
+  project_id          = "primus-infrastructure"
+  billing_account     = data.google_billing_account.pmqs_cloud_billing_account.id
+  auto_create_network = false
+  folder_id           = data.google_folder.primus.id
+}
