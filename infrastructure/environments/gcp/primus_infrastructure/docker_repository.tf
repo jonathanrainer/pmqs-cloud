340828d8b66118e9ef37e7a773fa60c7b8b8ab1f
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2024-08-04 21:37:34 +0100

    Remove `speculative` tag (#6)
    * Remove `speculative` tag
    
    * Add extra commentary

--- infrastructure/environments/gcp/primus_infrastructure/docker_repository.tf
+++ infrastructure/environments/gcp/primus_infrastructure/docker_repository.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "stone" {
   location      = "us-central1"
   repository_id = "stone"
-  description   = "A repository of the Docker Images required to run the Primus system"
+  description   = "A repository of the Docker Images required to run the services in Primus. Named after Sir Benjamin Stone."
   format        = "DOCKER"
 }
