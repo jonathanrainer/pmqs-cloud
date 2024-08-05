852c91c6d1d3f7671b5d0567702d5058e797f099
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2024-08-05 06:14:10 +0100

    Revert #8 and perform correct fix (#9)
    * Revert #8 and perform correct fix
    
    * Remove old refactoring block

--- infrastructure/environments/gcp/primus_infrastructure/project.tf
+++ infrastructure/environments/gcp/primus_infrastructure/project.tf
@@ -14,10 +14,5 @@ resource "google_project" "primus_infrastructure" {
   project_id          = data.google_client_config.this.project
   billing_account     = data.google_billing_account.pmqs_cloud_billing_account.id
   auto_create_network = false
-  folder_id           = data.google_folder.primus.name
-}
-
-moved {
-  from = google_project.pmqs_cloud_foundation
-  to   = google_project.primus_infrastructure
+  folder_id           = data.google_folder.primus.id
 }
