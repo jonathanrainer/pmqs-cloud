f7b11d28aabbd056643bec8349041618455ffc99
jonathanrainer <jonathan.rainer@apollographql.com>
2024-08-09 05:48:45 +0100

    Apply Terraform fmt to all Terraform files

--- infrastructure/environments/terraform_cloud/workspaces.tf
+++ infrastructure/environments/terraform_cloud/workspaces.tf
@@ -9,7 +9,7 @@ module "gcp_organisation_structure" {
   source   = "./modules/pmqs-cloud-workspace"
   for_each = toset(local.workspaces)
 
-  organization_name = tfe_organization.pmqs_cloud.name
-  workspace_name    = each.value
+  organization_name                 = tfe_organization.pmqs_cloud.name
+  workspace_name                    = each.value
   workload_identity_variable_set_id = tfe_variable_set.gcp_workload_identity_federation.id
 }
