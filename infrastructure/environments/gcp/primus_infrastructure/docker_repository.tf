f339355922d10f1766cd66809176f3084c816dfb
jonathanrainer <jonathan.rainer@apollographql.com>
2024-08-09 06:10:18 +0100

    Update Artifact Registry

--- infrastructure/environments/gcp/primus_infrastructure/docker_repository.tf
+++ infrastructure/environments/gcp/primus_infrastructure/docker_repository.tf
@@ -4,4 +4,7 @@ resource "google_artifact_registry_repository" "stone" {
   description   = "A repository of the Docker Images required to run the services in Primus. Named after Sir Benjamin Stone."
   format        = "DOCKER"
   project       = google_project.primus_infrastructure.project_id
+  docker_config {
+    immutable_tags = true
+  }
 }
