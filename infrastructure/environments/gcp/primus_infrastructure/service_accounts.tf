9cf48b1f464a4507cecebdacf99d994a765a7821
Jonathan Rainer <Jonathan@jonathan-rainer.com>
2024-08-10 09:58:21 +0100

    Fix post-apply errors (#21)

--- infrastructure/environments/gcp/primus_infrastructure/service_accounts.tf
+++ infrastructure/environments/gcp/primus_infrastructure/service_accounts.tf
@@ -2,5 +2,5 @@ resource "google_service_account" "github_actions_pmqs_cloud" {
   account_id   = "github-actions-pmqs-cloud"
   display_name = "GitHub Actions (pmqs-cloud)"
   description  = "The account through which GitHub Actions can publish artifacts"
-  project      = google_project.primus_infrastructure.id
+  project      = google_project.primus_infrastructure.project_id
 }
