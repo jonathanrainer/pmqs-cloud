97e23205b12ab87d98d0fdb62c4b63d21b5ff6ce
jonathanrainer <jonathan.rainer@apollographql.com>
2024-11-18 11:12:40 +0000

    Re-add Artifact Registry permissions

--- infrastructure/environments/gcp/primus_infrastructure/iam.tf
+++ infrastructure/environments/gcp/primus_infrastructure/iam.tf
@@ -2,10 +2,22 @@ data "google_iam_role" "security_reviewer" {
   name = "roles/iam.securityReviewer"
 }
 
+data "google_iam_role" "artifactregistry_administrator" {
+  name = "roles/artifactregistry.admin"
+}
+
 resource "google_project_iam_binding" "security_reviewer" {
   members = [
     "user:jonathan@pmqs.cloud"
   ]
   project = google_project.primus_infrastructure.project_id
   role    = data.google_iam_role.security_reviewer.name
+}
+
+resource "google_project_iam_binding" "artifactregistry_administrator" {
+  members = [
+    "user:jonathan@pmqs.cloud"
+  ]
+  project = google_project.primus_infrastructure.project_id
+  role    = data.google_iam_role.artifactregistry_administrator.name
 }
