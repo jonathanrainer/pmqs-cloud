21e516ba6e5012d2dba3110f1465ae22d6c01926
jonathanrainer <jonathan.rainer@apollographql.com>
2024-11-18 06:09:23 +0000

    Consolidate IAM permissions in a single role

--- infrastructure/environments/gcp/primus_infrastructure/iam.tf
+++ infrastructure/environments/gcp/primus_infrastructure/iam.tf
@@ -1,35 +1,11 @@
-data "google_iam_role" "artifactregistry_administrator" {
-  name = "roles/artifactregistry.admin"
+data "google_iam_role" "security_reviewer" {
+  name = "roles/iam.securityReviewer"
 }
 
-data "google_iam_role" "workload_identity_pool_viewer" {
-  name = "roles/iam.workloadIdentityPoolViewer"
-}
-
-data "google_iam_role" "cloudasset_viewer" {
-  name = "roles/cloudasset.viewer"
-}
-
-resource "google_project_iam_binding" "artifactregistry_administrator" {
+resource "google_project_iam_binding" "security_reviewer" {
   members = [
     "user:jonathan@pmqs.cloud"
   ]
   project = google_project.primus_infrastructure.project_id
-  role    = data.google_iam_role.artifactregistry_administrator.name
-}
-
-resource "google_project_iam_binding" "workload_identity_pool_viewer" {
-  members = [
-    "user:jonathan@pmqs.cloud"
-  ]
-  project = google_project.primus_infrastructure.project_id
-  role    = data.google_iam_role.workload_identity_pool_viewer.name
-}
-
-resource "google_project_iam_binding" "cloudasset_viewer" {
-  members = [
-    "user:jonathan@pmqs.cloud"
-  ]
-  project = google_project.primus_infrastructure.project_id
-  role    = data.google_iam_role.cloudasset_viewer.name
+  role    = data.google_iam_role.security_reviewer.name
 }
